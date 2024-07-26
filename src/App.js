eaaf2a172c406de81a18a5ce14fa0febb7347be1
Madhurima-Ghosh18 <ghoshmadhurima2002@gmail.com>
2024-07-26 10:01:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './Components/Home'
+import VideoCall from './Components/VideoCall'
 
-function App() {
+const App = () => {
+  const router=createBrowserRouter([
+    {
+      path:'/',
+      element:<Home/>
+    },
+    {
+      path:'/videocall/:roomID',
+      element:<VideoCall/>
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <RouterProvider router={router}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/variables.js
@@ -0,0 +1,2 @@
+export const APP_ID = 1710870998;
+export const SERVER_SECRET = "fcc84c77168e0644f5bce78dd2d2d120";
